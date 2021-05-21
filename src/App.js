ea6aa149fa0c7da3a62a888968509e2ff8477459
unknown <sumit.quarks@cars24.com>
2021-05-21 23:21:45 +0530

    google map api with react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react';
+import { GoogleApiWrapper,InfoWindow, Marker } from 'google-maps-react';
+import CurrentLocation from './Map';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+export const MapContainer=(props)=> {
+  const [showingInfoWindow,setShowingInfoWindow]=useState(false) 
+  const [activeMarker,setActiveMarker]=useState({})   
+   const [selectedPlace,setSelectedPlace]=useState({}) 
+
+
+   const onMarkerClick = (props, marker, e) =>{
+    setSelectedPlace(props)
+    setActiveMarker(marker)
+    setShowingInfoWindow(true)
+  }
+
+const  onClose = props => {
+   if (showingInfoWindow) {
+    setShowingInfoWindow(false)
+    setActiveMarker(null)
+   }
+ };
+
+    return (
+      <CurrentLocation
+        google={props.google}
+          initialCenter= {{
+            lat:28.644800,
+            lng:77.216721
+          }}
+          zoom={15}
+          centerAroundCurrentLocation={false}
+          visible={true}
+      >
+         <Marker
+          onClick={onMarkerClick}
+          name={'cars24'}
+        />
+        <InfoWindow
+          marker={activeMarker}
+          visible={showingInfoWindow}
+          onClose={onClose}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+          <div>
+            <h4>{selectedPlace.name}</h4>
+          </div>
+        </InfoWindow>
+        </CurrentLocation>
+    );
+  }
 
-export default App;
+
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyBFRLfm2hUnt3Qh63L1C6dheSyJL_kcsKk'
+})(MapContainer);
